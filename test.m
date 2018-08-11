535b762887b5b54fb8ffa9f1a62cfa20e045c129
Prashant Kumar <prash.k20@gmail.com>
2018-08-11 15:17:31 -0400

    added checks if image is not present, etc.

--- test.m
+++ test.m
@@ -1,3 +1,12 @@
-for n = 1242:1514
+tic
+for n = 1299:1514
     slic_stand_alone_fn(n)
 end
+toc
+
+%%
+output_folder2 = '/home/pkr/pkr-work/pose_estimation/build/segmentlabels/';
+
+images = dir(output_folder2);
+
+  
